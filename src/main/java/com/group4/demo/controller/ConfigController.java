e50880d07b8fee3c86d912d179209e065dfbf1f2
tmhbatw <1060226998@qq.com>
2020-12-30 20:48:43 +0800

    对大作业的后端代码进行了部分修改

--- src/main/java/com/group4/demo/controller/ConfigController.java
+++ src/main/java/com/group4/demo/controller/ConfigController.java
@@ -3,16 +3,20 @@ package com.group4.demo.controller;
 import com.group4.demo.Utility.PythonScriptExec;
 import org.python.util.PythonInterpreter;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
+
+@RestController
 public class ConfigController {
     @ResponseBody
-    @RequestMapping("/config")
+    @RequestMapping(value="/config",method= RequestMethod.POST)
     public Map<String,String> config(){
         String[] res=new PythonScriptExec().execPythonScript("config.py");
         Map<String,String> result=new HashMap<>();
@@ -20,6 +24,10 @@ public class ConfigController {
             result.put(i+"",res[i]);
         }
         return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println(new ConfigController().config());
     }
 
 }
