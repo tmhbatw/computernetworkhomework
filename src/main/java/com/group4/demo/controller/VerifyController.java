e3aac9ca9671b61fe182fe4900d8569ff63b7bd6
tmhbatw <1060226998@qq.com>
2021-01-06 18:27:14 +0800

    更新了验证的controller

--- src/main/java/com/group4/demo/controller/VerifyController.java
+++ src/main/java/com/group4/demo/controller/VerifyController.java
@@ -2,18 +2,28 @@ package com.group4.demo.controller;
 
 import com.group4.demo.Utility.PythonScriptExec;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class VerifyController {
     @ResponseBody
-    @RequestMapping("/verify")
-    public Map<String,String> verify(){
+    @RequestMapping(value="/verify",method= RequestMethod.POST)
+    public Map<String,String> config(){
         String[] res=new PythonScriptExec().execPythonScript("verify.py");
         Map<String,String> result=new HashMap<>();
-        result.put("result",res[0]);
+        for(int i=0;i<res.length;i++){
+            result.put(i+"",res[i]);
+        }
         return result;
     }
+
+    public static void main(String[] args){
+        System.out.println(new ConfigController().config());
+    }
+
 }
